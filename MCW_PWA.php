be43381b40740bdc6331e8c881f207d61cad9bdb
Yohan Totting <tyohan@gmail.com>
2018-11-19 18:33:44 +0700

    merge code

--- MCW_PWA.php
+++ MCW_PWA.php
@@ -34,20 +34,11 @@ define( 'MCW_SECTION_PERFORMANCE', 'mcw_option_performance' );
 define( 'MCW_SECTION_PWA', 'mcw_option_pwa' );
 define( 'MCW_PWA_SETTING_PAGE', 'mcw_setting_page' );
 
-<<<<<<< HEAD
-require_once( MCW_PWA_DIR . 'vendor/autoload.php' );
-require_once( MCW_PWA_DIR . 'includes/service_workers/MCW_PWA_Service_Worker.php' );
-require_once( MCW_PWA_DIR . 'includes/MCW_PWA_Settings.php' );
-require_once( MCW_PWA_DIR . 'includes/MCW_PWA_LazyLoad.php' );
-require_once( MCW_PWA_DIR . 'includes/MCW_PWA_Assets.php' );
-require_once( MCW_PWA_DIR . 'includes/MCW_PWA_Monitor.php' );
-=======
 require_once MCW_PWA_DIR . 'includes/service_workers/MCW_PWA_Service_Worker.php';
 require_once MCW_PWA_DIR . 'includes/MCW_PWA_Settings.php';
 require_once MCW_PWA_DIR . 'includes/MCW_PWA_LazyLoad.php';
 require_once MCW_PWA_DIR . 'includes/MCW_PWA_Assets.php';
 require_once MCW_PWA_DIR . 'includes/MCW_PWA_Monitor.php';
->>>>>>> b293fb5359f015a3e604d9cf952b1ba32b51ba90
 
 MCW_PWA_Settings::instance();
 MCW_PWA_Service_Worker::instance();
@@ -90,16 +81,6 @@ function mcw_init() {
 	if ( ! is_admin() ) {
 
 		MCW_PWA_Service_Worker::instance()->run();
-<<<<<<< HEAD
-		
-		//Disable some performance enhancement on AMP
-		//Try to support AMP for WP plugin https://github.com/ahmedkaludi/Accelerated-Mobile-Pages
-		// use AMPFORWP_AMP_QUERY_VAR
-		if ( defined( 'AMP_QUERY_VAR' ) && ! get_query_var( AMP_QUERY_VAR, false ) ) {
-				MCW_PWA_LazyLoad::instance()->run();
-				MCW_PWA_Assets::instance()->run();
-				
-=======
 
 		// Disable some performance enhancement on AMP.
 		// Try to support AMP for WP plugin https://github.com/ahmedkaludi/Accelerated-Mobile-Pages.
@@ -107,16 +88,12 @@ function mcw_init() {
 		if ( defined( 'AMP_QUERY_VAR' ) && ! get_query_var( AMP_QUERY_VAR, false ) ) {
 			MCW_PWA_LazyLoad::instance()->run();
 			MCW_PWA_Assets::instance()->run();
->>>>>>> b293fb5359f015a3e604d9cf952b1ba32b51ba90
 		}
 	}
 
 	MCW_PWA_Monitor::instance()->run();
 }
 
-<<<<<<< HEAD
-=======
 add_action( 'parse_query', 'mcw_init' );
 
->>>>>>> b293fb5359f015a3e604d9cf952b1ba32b51ba90
 
